98ca6240fe5b1029447b303177d9fdaaccb6ea41
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-10-03 18:42:43 +0300

    add botARmy function

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,7 +1,11 @@
 import React from "react";
+import BotCard from "./BotCard";
 
 function YourBotArmy() {
   //your bot army code here...
+  const botArmyCollection = botArmy.map((bot) => (
+    <BotCard key={bot.id} bot={bot} botArmy={botArmy} setBotArmy={setBotArmy} />
+  ))
 
   return (
     <div className="ui segment inverted olive bot-army">
@@ -9,6 +13,7 @@ function YourBotArmy() {
         <div className="row bot-army-row">
           {/*...and here...*/}
           Your Bot Army
+          {botArmyCollection}
         </div>
       </div>
     </div>
