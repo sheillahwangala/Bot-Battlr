23bc93d6432c1ff0e1a99d14fd3d37c12ffec89b
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-10-03 18:16:57 +0300

    add displayBotCard function

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,23 @@
 import React from "react";
+import BotCard from "./BotCard"
 
-function BotCollection() {
+function BotCollection({ bots, botArmy, setBotArmy, setBotData }) {
   // Your code here
+  const displayBotCards = bots.map((bot) => (
+    <BotCard 
+      key={bot.id} 
+      bot={bot} 
+      botArmy={botArmy} 
+      setBotArmy={setBotArmy}
+      setBotData={setBotData}
+    />  
+  ))
+
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
+        {displayBotCards}
         Collection of all bots
       </div>
     </div>
